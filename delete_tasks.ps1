68798cd2818ce71a8bf2bc57d59b5ccff85e19d2
roob.p <roberto_p88@yahoo.it>
2025-05-26 18:34:30 +0200

    Delete delete_tasks.ps1

--- delete_tasks.ps1
+++
@@ -1,7 +0,0 @@
-Unregister-ScheduledTask -TaskName "Sunshine DDX" -Confirm:$false
-Unregister-ScheduledTask -TaskName "Sunshine WGC" -Confirm:$false
-
-write "Tasks deleted"
-
-start-sleep -s 2
-exit
